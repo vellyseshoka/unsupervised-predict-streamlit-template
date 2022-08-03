63fd9d482cbc8732f99a1e15e99c17caabdde104
vellyseshoka <101458946+vellyseshoka@users.noreply.github.com>
2022-08-03 12:23:02 +0200

    Add files via upload

--- edsa_recommender_GM6.py
+++ edsa_recommender_GM6.py
@@ -88,9 +88,9 @@ def main():
 
         # User-based preferences
         st.write('### Enter Your Three Favorite Movies')
-        movie_1 = st.selectbox('Fisrt Option',title_list[1:500])
-        movie_2 = st.selectbox('Second Option',title_list[501:1000])
-        movie_3 = st.selectbox('Third Option',title_list[1001:1500])
+        movie_1 = st.selectbox('Fisrt Option',title_list[14930:15200])
+        movie_2 = st.selectbox('Second Option',title_list[25055:25255])
+        movie_3 = st.selectbox('Third Option',title_list[21100:21200])
         fav_movies = [movie_1,movie_2,movie_3]
 
         # Perform top-10 movie recommendation generation
